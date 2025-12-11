63fd69681c2019e751f00500e8d13f2301028bcd
jacobmontiel222 <jacobmontiel222@gmail.com>
2025-12-11 22:03:51 +0100

    iniciototop bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { NutritionProvider } from "@/contexts/NutritionContext";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import { ThemeProvider } from "next-themes";
